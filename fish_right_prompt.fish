b544869c4f833894cfddba06bddd5910c6795a76
Joe Standring <git@joestandring.com>
2021-10-22 19:22:37 +0100

    Remove brackets from git branch

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -5,11 +5,11 @@ set_color -b black
 set INREPO (git rev-parse --is-inside-work-tree 2>/dev/null)
 if test "$INREPO" = "true"
   set_color blue
-  printf '%s ' prompt_pwd   
+  printf '%s ' prompt_pwd
   set_color normal
   set_color -b purple
   set_color -o black
-  printf '%s' (fish_git_prompt)
+  printf ' %s' (git branch ^/dev/null | sed -n '/\* /s///p')
   set_color normal
   set_color purple
 else
