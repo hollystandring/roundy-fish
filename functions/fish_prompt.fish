02eb627922018005a07560e3dbd111ee6b4a4dd4
Panda <panda@Pandas-MacBook-Air.local>
2021-10-29 20:45:59 +0100

    Make functions... functions

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,19 +1,21 @@
-if test $status -eq 0
-  set EXIT_SYMBOL ' '
-  set_color blue
-else
-  set EXIT_SYMBOL ' '
-  set_color red
+function fish_prompt
+    if test $status -eq 0
+      set EXIT_SYMBOL ' '
+      set_color blue
+    else
+      set EXIT_SYMBOL ' '
+      set_color red
+    end
+    printf %s 
+    set_color -r
+    printf %s $EXIT_SYMBOL
+    set_color normal
+    set_color -b black
+    printf ' %s ' $USER
+    set_color -ro blue
+    printf ' %s' (date "+%H:%M:%S")
+    set_color normal
+    set_color blue
+    printf '%s ' 
+    set_color normal
 end
-printf %s 
-set_color -r
-printf %s $EXIT_SYMBOL
-set_color normal
-set_color -b black
-printf ' %s ' $USER
-set_color -ro blue
-printf ' %s' (date "+%H:%M:%S")
-set_color normal
-set_color blue
-printf '%s ' 
-set_color normal
--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,21 +1,23 @@
-set_color black
-printf %s 
-set_color normal
-set_color -b black
-set INREPO (git rev-parse --is-inside-work-tree 2>/dev/null)
-if test "$INREPO" = "true"
-  set_color blue
-  printf '%s ' (prompt_pwd)
-  set_color normal
-  set_color -b purple
-  set_color -o black
-  printf ' %s' (git branch ^/dev/null | sed -n '/\* /s///p')
-  set_color normal
-  set_color purple
-else
-  printf '%s' (prompt_pwd)
-  set_color normal
-  set_color black
+function fish_right_prompt
+    set_color black
+    printf %s 
+    set_color normal
+    set_color -b black
+    set INREPO (git rev-parse --is-inside-work-tree 2>/dev/null)
+    if test "$INREPO" = "true"
+      set_color blue
+      printf '%s ' (prompt_pwd)
+      set_color normal
+      set_color -b purple
+      set_color -o black
+      printf ' %s' (git branch ^/dev/null | sed -n '/\* /s///p')
+      set_color normal
+      set_color purple
+    else
+      printf '%s' (prompt_pwd)
+      set_color normal
+      set_color black
+    end
+    printf %s 
+    set_color normal
 end
-printf %s 
-set_color normal
